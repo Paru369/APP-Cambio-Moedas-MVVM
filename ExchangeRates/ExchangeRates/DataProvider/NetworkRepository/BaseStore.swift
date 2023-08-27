b7a40c635768e38e9e5b91687eea83a0ec378387
parutech <paruvitu@gmail.com>
2023-08-27 20:19:04 -0300

    Create BaseStore

---
+++ ExchangeRates/ExchangeRates/DataProvider/NetworkRepository/BaseStore.swift
@@ -0,0 +1,13 @@
+//
+//  BaseStore.swift
+//  ExchangeRates
+//
+//  Created by Paulo Pinheiro on 8/27/23.
+//
+
+import Foundation
+
+class BaseStore {
+    
+    let error = NSError(domain: "", code: 901, userInfo: [NSLocalizedDescriptionKey: " Error getting information"]) as Error
+}
