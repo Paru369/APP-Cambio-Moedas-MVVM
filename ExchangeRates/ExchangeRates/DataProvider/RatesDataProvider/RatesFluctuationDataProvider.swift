fc0edddbfc1732a41f213b8683d26443ba1d1554
parutech <paruvitu@gmail.com>
2023-08-28 12:44:09 -0300

    create dataProviderDelegate

---
+++ ExchangeRates/ExchangeRates/DataProvider/RatesDataProvider/RatesFluctuationDataProvider.swift
@@ -0,0 +1,8 @@
+//
+//  RatesFluctuationDataProvider.swift
+//  ExchangeRates
+//
+//  Created by Paulo Pinheiro on 8/28/23.
+//
+
+import Foundation
---
+++ ExchangeRates/ExchangeRates/DataProvider/RatesDataProvider/RatesHistoricalDataProvider.swift
@@ -0,0 +1,8 @@
+//
+//  RatesHistoricalDataProvider.swift
+//  ExchangeRates
+//
+//  Created by Paulo Pinheiro on 8/28/23.
+//
+
+import Foundation
