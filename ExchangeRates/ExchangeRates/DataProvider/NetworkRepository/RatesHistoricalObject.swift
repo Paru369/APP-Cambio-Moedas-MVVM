2a20978a169ba1df98913d892ab7a01724ff68ad
parutech <paruvitu@gmail.com>
2023-08-27 21:00:56 -0300

    Create RatesHistoricalObject

---
+++ ExchangeRates/ExchangeRates/DataProvider/NetworkRepository/RatesHistoricalObject.swift
@@ -0,0 +1,10 @@
+//
+//  RatesHistoricalObject.swift
+//  ExchangeRates
+//
+//  Created by Paulo Pinheiro on 8/27/23.
+//
+
+import Foundation
+
+typealias RatesHistoricalObject = [String: [String:String]]
