acd508caa94a0dea9c89f03718b131a279f3f60c
parutech <paruvitu@gmail.com>
2023-08-30 09:34:52 -0300

    fix TimeRangeEnum

--- ExchangeRates/ExchangeRates/Models/TimeRangeEnum.swift
+++ ExchangeRates/ExchangeRates/Models/TimeRangeEnum.swift
@@ -17,11 +17,11 @@ enum TimeRangeEnum {
     
     var date: Date {
         switch self {
-        case .today return Date(from: )
-        case .thisWeek
-        case .thisMonth
-        case .thisSemester
-        case .thisYear
+        case .today: return Date(from: .day, value:1)
+        case .thisWeek: return  Date(from: .day, value:6)
+        case .thisMonth: return  Date(from: .month, value:1)
+        case .thisSemester: return Date(from: .month, value:6)
+        case .thisYear: return  Date(from: .year, value:1)
             
         }
     }
