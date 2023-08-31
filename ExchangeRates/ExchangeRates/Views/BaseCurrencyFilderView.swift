7d1b1a02fa5776f6a610b98d2e660fa81506a4bb
parutech <paruvitu@gmail.com>
2023-08-31 19:55:50 -0300

    add base currency filter view

---
+++ ExchangeRates/ExchangeRates/Views/BaseCurrencyFilderView.swift
@@ -0,0 +1,8 @@
+//
+//  BaseCurrencyFilderView.swift
+//  ExchangeRates
+//
+//  Created by Paulo Pinheiro on 8/31/23.
+//
+
+import Foundation
---
+++ ExchangeRates/ExchangeRates/Views/CurrencySelectionFilterView.swift
@@ -0,0 +1,8 @@
+//
+//  CurrencySelectionFilterView.swift
+//  ExchangeRates
+//
+//  Created by Paulo Pinheiro on 8/31/23.
+//
+
+import Foundation
