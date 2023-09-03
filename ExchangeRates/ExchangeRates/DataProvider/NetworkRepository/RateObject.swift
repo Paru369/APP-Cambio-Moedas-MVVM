a4d162ddc2ffaa9ab4a3583271717f9efefb0b49
parutech <paruvitu@gmail.com>
2023-09-02 21:18:22 -0300

    create RateObject universal

---
+++ ExchangeRates/ExchangeRates/DataProvider/NetworkRepository/RateObject.swift
@@ -0,0 +1,33 @@
+//
+//  RateObject.swift
+//  ExchangeRates
+//
+//  Created by Paulo Pinheiro on 9/2/23.
+//
+
+import Foundation
+
+
+struct RateObject<Rates: Codable>: Codable {
+    var base: String?
+    var success: Bool = false
+    var rates: Rates?
+}
+
+typealias RatesFluctuationObject = [String: FluctuationObject]
+
+struct FluctuationObject: Identifiable, Codable {
+    
+    let id = UUID()
+    let change: Double
+    let changePct: Double
+    let endRate: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case change
+        case changePct = "change_pct"
+        case endRate = "end_rate"
+    }
+}
+
+typealias RatesHistoricalObject = [String: [String: Double]]
